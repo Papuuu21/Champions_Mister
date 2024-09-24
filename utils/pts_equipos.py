02b2d73c615268ad0e1fca8e156874b4b8d71c95
Papuuu21 <dealva.pablo@gmail.com>
2024-09-24 14:59:08 +0200

    Actualizacion de jornada y clasificacion, eliminacion de funciones y archivos no utiles

---
+++ utils/pts_equipos.py
@@ -0,0 +1,18 @@
+
+ #* PTS EQUIPOS JORNADAS:
+def pts_equipos():
+    palop = int(input('Pts Palop: '))
+    fale = int(input('Pts Fale: ')) 
+    tony = int(input('Pts Tony: '))
+    lope = int(input('Pts Lope: '))
+    ruso = int(input('Pts Ruso: '))
+    coquina = int(input('Pts Coquina: '))
+    papu = int(input('Pts Papu: ')) 
+    kike = int(input('Pts Kike: '))
+    gonzo = int(input('Pts Gonzo: '))
+    diego = int(input('Pts Diego: '))
+    kero = int(input('Pts Kero: ')) 
+    armada = int(input('Pts Armada: '))
+
+    return palop, fale, tony, lope, ruso, coquina, papu, kike, gonzo, diego, kero, armada
+
