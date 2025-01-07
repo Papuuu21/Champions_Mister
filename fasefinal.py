622916a748e558efc39257fd183dfed93be5f249
Papuuu21 <dealva.pablo@gmail.com>
2025-01-07 19:19:11 +0100

    Preparada la fase final para empezar la eliminatoria

--- fasefinal.py
+++ fasefinal.py
@@ -2,17 +2,9 @@ import pandas as pd
 from utils.pts_equipos import pts_equipos
 
 
-clasificados = [[0, '4ºGB', '1ºGB', 0, '||', 0, '3ºGB', '1ºGA', 0], [0, '2ºGB', '2ºGA', 0, '||', 0, '3ºGA', '4ºGA', 0]]
+clasificados = [[0, 'ARMADA', 'PAPU', 0, '||', 0, 'PUCHE', 'KERO', 0], [0, 'GONZO', 'PALOP', 0, '||', 0, 'LOPE', 'RUSO', 0]]
 
 columnas = ['PTOS LA', 'LOCAL A', 'VISITANTE A', 'PTOS VA', '||', 'PTOS LB', 'LOCAL B',  'VISITANTE B', 'PTOS VB']
 cuartos = pd.DataFrame(data= clasificados, index=[1,2], columns=columnas)
 print(cuartos)
 
- #!Rtdos Jornada actualizados: BORRAR A LOS JUGADORES ELIMINADOS
-palop, fale, tony, lope, ruso, coquina, papu, kike, gonzo, puche, kero, armada = pts_equipos()
-resultados = {
-    'Lope': lope, 'Palop': palop, 'Fale': fale, 'Kero': kero, 'Ruso': ruso, 'Tony': tony,
-    'Puche': puche, 'Coquina': coquina, 'Kike': kike, 'Armada': armada, 'Papu': papu, 'Gonzo': gonzo
-}
-
-print(resultados)
--- streamlit.py
+++ streamlit.py
@@ -2,32 +2,31 @@
 
 import pandas as pd
 import  streamlit as st
-c_ini_gA = pd.read_excel('data/clasf_jor10_gA.xlsx')
-c_ini_gB = pd.read_excel('data/clasf_jor10_gB.xlsx')
+from PIL import Image
+
+c_ini_gA = pd.read_excel('data/clasf_final_gA.xlsx')
+c_ini_gB = pd.read_excel('data/clasf_final_gB.xlsx')
 calendario = pd.read_excel('data/calendario_grupos.xlsx')
-jor_actual = pd.read_excel('data/jornada_10_inicial.xlsx')
+jor_actual = pd.read_excel('data/cuartos.xlsx')
 jor_pasada = pd.read_excel('data/j10_actualizada.xlsx')
 
 # Iniciar app con Streamlit
 st.title('CHAMPIONS MISTER 🏆⚽')
 
-from PIL import Image
-
+st.header('----FASE FINAL----')
 #* Cargar y mostrar la imagen al inicio
-image = Image.open('data/Sorteo_posiciones_cuartos.jpg')
+image = Image.open('data/Cuartos_CUADRO.png')
 st.image(image, caption='Sorteo fase final')
 
 st.header('----Fase de Grupos----')
-
-st.subheader('Clasificación Jornada 10')
+st.subheader('Clasificación Final Grupos')
 st.write('**Grupo A**')
 st.dataframe(c_ini_gA)
-
 st.write('**Grupo B**')
 st.dataframe(c_ini_gB)
 
-st.header('Emparejamientos Cuartos de final')
-st.write('**EMPAREJAMIENTOS CUARTOS PENDIENTE DE LOS PARTIDOS APLAZADOS --> CUARTOS JORNADA 20**')
+st.header('Resultados Cuartos de final')
+st.dataframe(jor_actual)
 
 st.header('Jornada Anterior')
 st.dataframe(jor_pasada)
